234f100563e913c3008748e7cad614ffb027ce87
Kevin McCartney <hello@kevinmccartney.is>
2016-03-15 03:59:17 -0700

    final refactoring

--- functions.php
+++ functions.php
@@ -4,15 +4,15 @@
  *
  * @since Blvd 2.7.0
  */
-if ( !defined( 'HJI_BLVD_SCSS' ) ) {
+if( !defined( 'HJI_BLVD_SCSS' ) ) :
     define( 'HJI_BLVD_SCSS', true );
-}
+endif;
 
 /**
  * registering the fullwidth quicksearch area & deregistering a few 
  * uneeded widget areas
  */
-if ( !function_exists( 'hji_buyers_realty_widgets_init' ) ) {
+if( !function_exists( 'hji_buyers_realty_widgets_init' ) ) :
     function hji_buyers_realty_widgets_init() {
         register_sidebar( array(
             'id'            => 'fullwidth_qs',
@@ -29,24 +29,24 @@ if ( !function_exists( 'hji_buyers_realty_widgets_init' ) ) {
         unregister_sidebar( 'blvd-topbar-sidebarwidgets' );
     }
     add_action( 'widgets_init', 'hji_buyers_realty_widgets_init' );
-}
+endif;
 
 /**
  * Loading in extra fonts
  */
-if ( !function_exists( 'hji_buyers_realty_load_fonts' ) ) {
+if( !function_exists( 'hji_buyers_realty_load_fonts' ) ) :
     function hji_buyers_realty_load_fonts() {
         wp_enqueue_style( 'googleFonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600' );
     }
     add_action( 'wp_enqueque_scripts', 'hji_buyers_realty_load_fonts' );
-}
+endif;
 
 /**
  * loading in child theme js
  */
-if ( !function_exists( 'hji_buyers_realty_load_scripts' ) ) {
+if( !function_exists( 'hji_buyers_realty_load_scripts' ) ) :
     function hji_buyers_realty_load_scripts() {
         wp_enqueue_script( 'hji_buyers_realty_scripts', get_stylesheet_directory_uri() . '/assets/js/main.js' );
     }
-    add_action( 'wp_enqueue_scripts', 'hji_buyers_realty_load_scripts', 11 );
-}
+    add_action( 'wp_enqueue_scripts', 'hji_buyers_realty_load_scripts' );
+endif;
